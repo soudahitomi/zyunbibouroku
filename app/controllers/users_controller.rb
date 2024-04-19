29c55705b500c0edb8e96c1f49aeac4d2dc5f543
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-04-19 12:23:02 +0000

    ユーザーのデフォルトの画像設定、編集機能完了

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+
+  def update
+    @user = User.find(params[:id])
+    @user.update
+    redirect_to root_path
+  end
+  def edit
+    @user = User.find(params[:id])
+  end
+end
