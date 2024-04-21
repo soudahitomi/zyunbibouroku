bd6500cc3d8124590eeb00403afa913eeca109a7
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-04-21 11:54:07 +0000

    ゲストログインのエラーの解消

--- app/controllers/user/sessions_controller.rb
+++
@@ -1,9 +0,0 @@
-class Uers::SessionsController < Devise::SessionsController
-  
-  def guest_sign_in
-    user = User.guest
-    sign_in user
-    redirect_to user_path(user), notice: "guestuserでログインしました。"
-  end
-  
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,13 @@ class User < ApplicationRecord
   GUEST_USER_EMAIL = "guest@example.com"
 
   def self.guest
-    find_or_cerate_by!(email: GUEST_USER_EMAIL) do |user|
+    find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
       user.password = SecureRandom.urlsafe_base64
       user.name = "guestuser"
     end
   end
-  
+
+
   def guest_user?
     email == GUEST_USER_EMAIL
   end
