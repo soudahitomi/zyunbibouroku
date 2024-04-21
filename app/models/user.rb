a7f4350f62e20690e841803217607836e3228189
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-04-21 09:41:28 +0000

    ゲストログイン機能のみ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,19 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
-  def get_profile_image(width, height)
-  unless profile_image.attached?
-    file_path = Rails.root.join('app/assets/images/no_image.jpg')
-    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-  end
-  profile_image.variant(resize_to_limit: [width, height]).processed
-end
+  GUEST_USER_EMAIL = "guest@example.com"
 
+  def self.guest
+    find_or_cerate_by!(email: GUEST_USER_EMAIL) do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+  end
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
 end
