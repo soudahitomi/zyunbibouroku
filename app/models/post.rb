d545907f710d3240cb71031ef31a4d71da298aac
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-04-23 12:49:26 +0000

    postのnewのviewがまだできていない

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :lists, dependent: :destroy
-  accepts_nested_attributes_for :lists
+  accepts_nested_attributes_for :lists, allow_destroy: true
 end
