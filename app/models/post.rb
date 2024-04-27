20fe235fe86d7a066a78a71a4ee40dc60c019d3f
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-04-27 09:25:17 +0000

    いいねモデル作成、アソシエーション完了

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ApplicationRecord
   has_many :lists, dependent: :destroy
   accepts_nested_attributes_for :lists, allow_destroy: true
   has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_26_101556) do
+ActiveRecord::Schema.define(version: 2024_04_27_091543) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 2024_04_26_101556) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
     t.datetime "created_at", precision: 6, null: false
