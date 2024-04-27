4a2f846b19fffbaee2063ee9f4ccf741c074f6c3
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-04-27 08:57:19 +0000

    コメント機能、view確認済み　投稿詳細ページ確認済み

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,9 +3,9 @@ class CommentsController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     comment = current_user.comments.new(comment_params)
-    comment.post_id = post_id
+    comment.post_id = post.id
     comment.save
-    redirect_to root_path
+    redirect_to post_path(post.id)
   end
 
   def destroy
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @list = List.find(params[:id])
     @comment = Comment.new
   end
 
