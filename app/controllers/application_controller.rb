939c673dba970c6afcf488c9459ab65df7467709
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-05-21 12:51:57 +0000

    エンドユーザー側のDeviseController作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,9 @@
-class Public::ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base
   before_action :authenticate_user!, unless: :admin_controller?
-  before_action :configre_permitted_parameters, if: :devise_controller?
-
-  def after_sign_in_path_for(resource)
-    posts_path
-  end
-
-  def after_sign_out_path_for(resource)
-    posts_path
-  end
 
   private
 
   def admin_controller?
     self.class.module_parent_name == 'Admin'
-  end
-
-  def configre_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
   end
 end
