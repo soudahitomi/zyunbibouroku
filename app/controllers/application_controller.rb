39f02a6ab37c3d18c6475f6f3efbd1d6e8f461ee
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-05-21 13:04:09 +0000

    ApplicationControllerでUserとAdminの権限を振り分ける

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, unless: :admin_controller?
+  before_action :configure_authentication
 
   private
 
+  def configure_authentication
+    if admin_controller?
+      authenticate_admin!
+    else
+      authenticate_user! unless action_is_public?
+    end
+  end
+
   def admin_controller?
     self.class.module_parent_name == 'Admin'
+  end
+
+  def action_is_public?
+    controller_name == 'posts' && action_name == 'index'
   end
 end
