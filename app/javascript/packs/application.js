30eedba2dddd85629f4ffebc626006c8c9257ab4
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-05-27 09:14:45 +0000

    サインイン、ログイン画面レイアウト調整

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,11 +24,11 @@ ActiveStorage.start()
 
 // 無限スクロールの処理
 var jscrollOption = {
-    loadingHtml: '読み込み中・・・', //記事読み込み中の表示
-    autoTrigger: true, // 自動で読み込むか否か、trueで自動、falseでボタンクリックとなる。
-    padding: 20, // 指定したコンテンツの下かた何pxで読み込むかを指定(autoTrigger: trueの場合のみ)
-     nextSelector: '.next a',
-    contentSelector: '.jscroll' // 読み込む範囲の指定
+    loadingHtml: '読み込み中・・・',
+    autoTrigger: true,
+    padding: 20,
+     nextSelector: 'span.next:last a',//.next a これでもできた
+    contentSelector: '.jscroll'
 
 };
 $(document).on('turbolinks:load', function() {
