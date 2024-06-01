b76cb2387d558754d717c99bc4277c4f2ab7f3c1
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-06-01 15:47:20 +0000

    通知一覧レイアウト完成

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -1,6 +1,6 @@
 class Public::NotificationsController < ApplicationController
     def index
-      @notifications = current_user.notifications.order(create_at: :desc)#.page(params[:page]).per(20)kaminari入れた後追記する
+      @notifications = current_user.notifications.order(create_at: :desc).page(params[:page]).per(10)
     end
 
     def update
