cb1ee94cd1e4b7eefad8d157f8b6d77a057c551a
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-06-03 09:51:58 +0000

    users/favoriteレイアウト完成

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -19,6 +19,7 @@ class Public::UsersController < ApplicationController
 
   def favorites
     @user = User.find(params[:id])
+    @posts = current_user.posts.page(params[:page]).per(8)
     favorites = Favorite.where(user_id: @user.id).pluck(:post_id)
     @favorite_posts = Post.find(favorites)
   end
