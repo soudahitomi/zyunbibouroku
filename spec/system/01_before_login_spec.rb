2341989b6f111d08cb670469017d2f2324ad5ab9
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-06-12 10:19:54 +0000

    ログイン前systemテスト完了

---
+++ spec/system/01_before_login_spec.rb
@@ -0,0 +1,169 @@
+require 'rails_helper'
+
+describe '[STEP1] ユーザログイン前のテスト' do
+  describe 'トップ画面のテスト' do
+    before do
+      visit root_path
+    end
+
+    context '表示内容の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/'
+      end
+      it 'Log inリンクが表示される: ボタンの表示が「ログイン」である' do
+        log_in_link = find_all('a')[1].text
+        expect(log_in_link).to match(/ログイン/)
+      end
+      it 'Log inリンクの内容が正しい' do
+        log_in_link = find_all('a')[1].text
+        expect(page).to have_link log_in_link, href: new_user_session_path
+      end
+      it 'Sign upリンクが表示される: ボタンの表示が「新規登録」である' do
+        sign_up_link = find_all('a')[0].text
+        expect(sign_up_link).to match(/新規登録/)
+      end
+      it 'Sign upリンクの内容が正しい' do
+        sign_up_link = find_all('a')[0].text
+        expect(page).to have_link sign_up_link, href: new_user_registration_path
+      end
+    end
+  end
+
+  describe 'ユーザ新規登録のテスト' do
+    before do
+      visit new_user_registration_path
+    end
+
+    context '表示内容の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/users/sign_up'
+      end
+      it '「新規登録」と表示される' do
+        expect(page).to have_content '新規登録'
+      end
+      it 'nameフォームが表示される' do
+        expect(page).to have_field 'user[name]'
+      end
+      it 'emailフォームが表示される' do
+        expect(page).to have_field 'user[email]'
+      end
+      it 'passwordフォームが表示される' do
+        expect(page).to have_field 'user[password]'
+      end
+      it 'password_confirmationフォームが表示される' do
+        expect(page).to have_field 'user[password_confirmation]'
+      end
+      it '登録するボタンが表示される' do
+        expect(page).to have_button '登録する'
+      end
+    end
+
+    context '新規登録成功のテスト' do
+      before do
+        fill_in 'user[name]', with: Faker::Lorem.characters(number: 10)
+        fill_in 'user[email]', with: Faker::Internet.email
+        fill_in 'user[password]', with: 'password'
+        fill_in 'user[password_confirmation]', with: 'password'
+      end
+
+      it '正しく新規登録される' do
+        expect { click_button '登録する' }.to change(User.all, :count).by(1)
+      end
+      it '新規登録後のリダイレクト先が、投稿一覧画面になっている' do
+        click_button '登録する'
+        expect(current_path).to eq '/posts'
+      end
+    end
+  end
+
+  describe 'ユーザログイン' do
+    let(:user) { create(:user) }
+
+    before do
+      visit new_user_session_path
+    end
+
+    context '表示内容の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/users/sign_in'
+      end
+      it '「ログイン」と表示される' do
+        expect(page).to have_content 'ログイン'
+      end
+      it 'nameフォームが表示される' do
+        expect(page).to have_field 'user[name]'
+      end
+      it 'passwordフォームが表示される' do
+        expect(page).to have_field 'user[password]'
+      end
+      it 'ログインボタンが表示される' do
+        expect(page).to have_button 'ログイン'
+      end
+      it 'emailフォームは表示されない' do
+        expect(page).not_to have_field 'user[email]'
+      end
+    end
+
+    context 'ログイン成功のテスト' do
+      before do
+        fill_in 'user[name]', with: user.name
+        fill_in 'user[password]', with: user.password
+        click_button 'ログイン'
+      end
+
+      it 'ログイン後のリダイレクト先が、投稿一覧画面になっている' do
+        expect(current_path).to eq '/posts'
+      end
+    end
+
+    context 'ログイン失敗のテスト' do
+      before do
+        fill_in 'user[name]', with: ''
+        fill_in 'user[password]', with: ''
+        click_button 'ログイン'
+      end
+
+      it 'ログインに失敗し、ログイン画面にリダイレクトされる' do
+        expect(current_path).to eq '/users/sign_in'
+      end
+    end
+  end
+
+  describe 'ヘッダーのテスト: ログインしている場合' do
+    let(:user) { create(:user) }
+
+    before do
+      visit new_user_session_path
+      fill_in 'user[name]', with: user.name
+      fill_in 'user[password]', with: user.password
+      click_button 'ログイン'
+    end
+
+    context 'ヘッダーの表示を確認' do
+      it 'Log outリンクが表示される' do
+        logout_link = find_all('a')[1].text
+        expect(logout_link).to match(/ログアウト/)
+      end
+    end
+  end
+
+  describe 'ユーザログアウトのテスト' do
+    let(:user) { create(:user) }
+
+    before do
+      visit new_user_session_path
+      fill_in 'user[name]', with: user.name
+      fill_in 'user[password]', with: user.password
+      click_button 'ログイン'
+      logout_link = find_all('a')[1].text
+      logout_link = logout_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
+      click_link logout_link
+    end
+
+    context 'ログアウト機能のテスト' do
+      it 'ログアウト後のリダイレクト先が、トップになっている' do
+        expect(current_path).to eq '/'
+      end
+    end
+  end
+end
