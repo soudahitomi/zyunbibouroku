fbb750eedb3a0703ac5c27b21eff193b36695285
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-06-12 03:59:53 +0000

    Notificationモデルテスト完了

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,3 +1,14 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+
+RSpec.describe 'Notificationモデルのテスト', type: :model do
+
+  describe 'アソシエーションのテスト' do
+    context 'Userモデルとの関係' do
+      it 'N:1となっている', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
+        expect(Notification.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+  end
+end
