b05ca5ac13ff8f2b380466f1b1f01ad4cc7a5f25
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-06-14 14:31:58 +0000

    ログイン後テスト仮完了

--- spec/system/02_after_login_spec.rb
+++ spec/system/02_after_login_spec.rb
@@ -5,6 +5,7 @@ describe '[STEP2] ユーザログイン後のテスト' do
   let!(:other_user) { create(:user) }
   let!(:post) { create(:post, user: user) }
   let!(:other_post) { create(:post, user: other_user) }
+  let!(:list) { create(:list, post: post) }
 
   before do
     visit new_user_session_path
@@ -61,7 +62,7 @@ describe '[STEP2] ユーザログイン後のテスト' do
     before do
       visit new_post_path
       fill_in 'post[title]', with: Faker::Lorem.characters(number: 5)
-      #fill_in 'list[content]', with: Faker::Lorem.characters(number: 20)
+      #fill_in 'list[content]', with: Faker::Lorem.characters(number: 20        )#1
     end
 
     context '投稿成功のテスト' do
@@ -76,7 +77,7 @@ describe '[STEP2] ユーザログイン後のテスト' do
   end#投稿画面のテスト'
 
   describe '自分の投稿詳細画面のテスト' do
-    before do#2ここで投稿の情報とかって入れないの？
+    before do                                                                     #2ここで投稿の情報とかって入れないの？
       visit post_path(post)
     end
 
@@ -91,7 +92,7 @@ describe '[STEP2] ユーザログイン後のテスト' do
         expect(page).to have_content post.title
       end
       # it '投稿のlistが表示される' do
-      #   expect(page).to have_content list.content
+      #   expect(page).to have_content list.content                             #1
       # end
       it '投稿の編集リンクが表示される' do
         expect(page).to have_link '投稿を編集する', href: edit_post_path(post)
@@ -124,7 +125,7 @@ describe '[STEP2] ユーザログイン後のテスト' do
         expect(page).to have_link '', href: user_followers_path(user)
       end
     end
-# kokokara
+
     context '編集リンクのテスト' do
       it '編集画面に遷移する' do
         second_user_post = FactoryBot.create(:post, user: user)
@@ -157,108 +158,47 @@ describe '[STEP2] ユーザログイン後のテスト' do
       end
     end
   end#自分の投稿詳細画面のテスト
-# korekesu
+
   describe '自分の投稿編集画面のテスト' do
     before do
-      visit edit_book_path(book)
+      visit edit_post_path(post)
     end
 
     context '表示の確認' do
-      it 'URLが正しい', spec_category: "CRUD機能に対するコントローラの処理と流れ(ログイン状況を意識した応用)" do
-        expect(current_path).to eq '/books/' + book.id.to_s + '/edit'
+      it 'URLが正しい' do
+        expect(current_path).to eq '/posts/' + post.id.to_s + '/edit'
       end
-      it '「Editing Book」と表示される', spec_category: "CRUD機能に対するコントローラの処理と流れ(ログイン状況を意識した応用)" do
-        expect(page).to have_content 'Editing Book'
+      it 'title編集フォームが表示される' do
+        expect(page).to have_field 'post[title]', with: post.title
       end
-      it 'title編集フォームが表示される', spec_category: "CRUD機能に対するコントローラの処理と流れ(ログイン状況を意識した応用)" do
-        expect(page).to have_field 'book[title]', with: book.title
-      end
-      it 'body編集フォームが表示される', spec_category: "CRUD機能に対するコントローラの処理と流れ(ログイン状況を意識した応用)" do
-        expect(page).to have_field 'book[body]', with: book.body
-      end
-      it 'Update Bookボタンが表示される', spec_category: "CRUD機能に対するコントローラの処理と流れ(ログイン状況を意識した応用)" do
-        expect(page).to have_button 'Update Book'
-      end
-      it 'Showリンクが表示される', spec_category: "CRUD機能に対するコントローラの処理と流れ(ログイン状況を意識した応用)" do
-        expect(page).to have_link 'Show', href: book_path(book)
-      end
-      it 'Backリンクが表示される', spec_category: "CRUD機能に対するコントローラの処理と流れ(ログイン状況を意識した応用)" do
-        expect(page).to have_link 'Back', href: books_path
+      # it 'list編集フォームが表示される' do
+      #   expect(page).to have_field 'list[content]', with: list.content        #1
+      # end
+      it '更新するボタンが表示される' do
+        expect(page).to have_button '更新する'
       end
     end
 
     context '編集成功のテスト' do
       before do
-        @book_old_title = book.title
-        @book_old_body = book.body
-        fill_in 'book[title]', with: Faker::Lorem.characters(number: 4)
-        fill_in 'book[body]', with: Faker::Lorem.characters(number: 19)
-        click_button 'Update Book'
+        @post_old_title = post.title
+        #@list_old_content = list.content     #post.list.content?                 1
+        fill_in 'post[title]', with: Faker::Lorem.characters(number: 4)
+        #fill_in 'list[content]', with: Faker::Lorem.characters(number: 19)       1
+        click_button '更新する'
       end
 
-      it 'titleが正しく更新される', spec_category: "CRUD機能に対するコントローラの処理と流れ(ログイン状況を意識した応用)" do
-        expect(book.reload.title).not_to eq @book_old_title
+      it 'titleが正しく更新される' do
+        expect(post.reload.title).not_to eq @post_old_title
       end
-      it 'bodyが正しく更新される', spec_category: "CRUD機能に対するコントローラの処理と流れ(ログイン状況を意識した応用)" do
-        expect(book.reload.body).not_to eq @book_old_body
-      end
-      it 'リダイレクト先が、更新した投稿の詳細画面になっている', spec_category: "CRUD機能に対するコントローラの処理と流れ(ログイン状況を意識した応用)" do
-        expect(current_path).to eq '/books/' + book.id.to_s
-        expect(page).to have_content 'Book detail'
-      end
-    end
-  end
-
-  describe 'ユーザ一覧画面のテスト' do
-    before do
-      visit users_path
-    end
-
-    context '表示内容の確認' do
-      it 'URLが正しい', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(current_path).to eq '/users'
-      end
-      it '自分と他人の画像が表示される: fallbackの画像がサイドバーの1つ＋一覧(2人)の2つの計3つ存在する', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(all('img').size).to eq(3)
-      end
-      it '自分と他人の名前がそれぞれ表示される', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(page).to have_content user.name
-        expect(page).to have_content other_user.name
-      end
-      it '自分と他人のshowリンクがそれぞれ表示される', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(page).to have_link 'Show', href: user_path(user)
-        expect(page).to have_link 'Show', href: user_path(other_user)
+      # it 'contentが正しく更新される' do
+      #   expect(book.reload.body).not_to eq @list_old_content                    1
+      # end
+      it 'リダイレクト先が、投稿一覧画面になっている' do
+        expect(current_path).to eq '/posts'
       end
     end
-
-    context 'サイドバーの確認' do
-      it '自分の名前と紹介文が表示される', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(page).to have_content user.name
-        expect(page).to have_content user.introduction
-      end
-      it '自分のユーザ編集画面へのリンクが存在する', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(page).to have_link '', href: edit_user_path(user)
-      end
-      it '「New book」と表示される', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(page).to have_content 'New book'
-      end
-      it 'titleフォームが表示される', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(page).to have_field 'book[title]'
-      end
-      it 'titleフォームに値が入っていない', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(find_field('book[title]').text).to be_blank
-      end
-      it 'bodyフォームが表示される', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(page).to have_field 'book[body]'
-      end
-      it 'bodyフォームに値が入っていない', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(find_field('book[body]').text).to be_blank
-      end
-      it 'Create Bookボタンが表示される', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(page).to have_button 'Create Book'
-      end
-    end
-  end
+  end#自分の投稿編集画面のテスト
 
   describe '自分のユーザ詳細画面のテスト' do
     before do
@@ -266,96 +206,52 @@ describe '[STEP2] ユーザログイン後のテスト' do
     end
 
     context '表示の確認' do
-      it 'URLが正しい', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
+      it 'URLが正しい' do
         expect(current_path).to eq '/users/' + user.id.to_s
       end
-      it '投稿一覧のユーザ画像のリンク先が正しい', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(page).to have_link '', href: user_path(user)
+      it '投稿一覧に自分の投稿のtitleが表示され、リンクが正しい' do
+        expect(page).to have_link post.title, href: post_path(post)
       end
-      it '投稿一覧に自分の投稿のtitleが表示され、リンクが正しい', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(page).to have_link book.title, href: book_path(book)
-      end
-      it '投稿一覧に自分の投稿のbodyが表示される', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(page).to have_content book.body
-      end
-      it '他人の投稿は表示されない', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(page).not_to have_link '', href: user_path(other_user)
-        expect(page).not_to have_content other_book.title
-        expect(page).not_to have_content other_book.body
-      end
-    end
-
-    context 'サイドバーの確認' do
-      it '自分の名前と紹介文が表示される', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(page).to have_content user.name
-        expect(page).to have_content user.introduction
-      end
-      it '自分のユーザ編集画面へのリンクが存在する', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(page).to have_link '', href: edit_user_path(user)
-      end
-      it '「New book」と表示される', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(page).to have_content 'New book'
-      end
-      it 'titleフォームが表示される', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(page).to have_field 'book[title]'
-      end
-      it 'titleフォームに値が入っていない', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(find_field('book[title]').text).to be_blank
-      end
-      it 'bodyフォームが表示される', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(page).to have_field 'book[body]'
-      end
-      it 'bodyフォームに値が入っていない', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(find_field('book[body]').text).to be_blank
-      end
-      it 'Create Bookボタンが表示される', spec_category: "基本的なアソシエーション概念と適切な変数設定" do
-        expect(page).to have_button 'Create Book'
-      end
+      # it '投稿一覧に自分の投稿listのcontentが表示される' do                   1
+      #   expect(page).to have_content list.content
+      # end
     end
   end
-
+# koko
   describe '自分のユーザ情報編集画面のテスト' do
     before do
       visit edit_user_path(user)
     end
 
     context '表示の確認' do
-      it 'URLが正しい', spec_category: "CRUD機能に対するコントローラの処理と流れ(ログイン状況を意識した応用)" do
+      it 'URLが正しい' do
         expect(current_path).to eq '/users/' + user.id.to_s + '/edit'
       end
-      it '名前編集フォームに自分の名前が表示される', spec_category: "CRUD機能に対するコントローラの処理と流れ(ログイン状況を意識した応用)" do
+      it '名前編集フォームに自分の名前が表示される' do
         expect(page).to have_field 'user[name]', with: user.name
       end
-      it '画像編集フォームが表示される', spec_category: "CRUD機能に対するコントローラの処理と流れ(ログイン状況を意識した応用)" do
+      it '画像編集フォームが表示される' do
         expect(page).to have_field 'user[profile_image]'
       end
-      it '自己紹介編集フォームに自分の自己紹介文が表示される', spec_category: "CRUD機能に対するコントローラの処理と流れ(ログイン状況を意識した応用)" do
-        expect(page).to have_field 'user[introduction]', with: user.introduction
-      end
-      it 'Update Userボタンが表示される', spec_category: "CRUD機能に対するコントローラの処理と流れ(ログイン状況を意識した応用)" do
-        expect(page).to have_button 'Update User'
+      it '登録ボタンが表示される' do
+        expect(page).to have_button '登録'
       end
     end
 
     context '更新成功のテスト' do
       before do
         @user_old_name = user.name
-        @user_old_intrpduction = user.introduction
         fill_in 'user[name]', with: Faker::Lorem.characters(number: 9)
-        fill_in 'user[introduction]', with: Faker::Lorem.characters(number: 19)
         expect(user.profile_image).to be_attached
-        click_button 'Update User'
+        click_button '登録'
         save_page
       end
 
-      it 'nameが正しく更新される', spec_category: "CRUD機能に対するコントローラの処理と流れ(ログイン状況を意識した応用)" do
+      it 'nameが正しく更新される' do
         expect(user.reload.name).not_to eq @user_old_name
       end
-      it 'introductionが正しく更新される', spec_category: "CRUD機能に対するコントローラの処理と流れ(ログイン状況を意識した応用)" do
-        expect(user.reload.introduction).not_to eq @user_old_intrpduction
-      end
-      it 'リダイレクト先が、自分のユーザ詳細画面になっている', spec_category: "CRUD機能に対するコントローラの処理と流れ(ログイン状況を意識した応用)" do
-        expect(current_path).to eq '/users/' + user.id.to_s
+      it 'リダイレクト先が、投稿一覧画面になっている', spec_category: "CRUD機能に対するコントローラの処理と流れ(ログイン状況を意識した応用)" do
+        expect(current_path).to eq '/posts'
       end
     end
   end
