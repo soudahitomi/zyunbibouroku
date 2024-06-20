d807953c213008d6c2365b5c5b657e3a6e445c44
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-06-20 13:05:48 +0000

    User,Postのテストデータ挿入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+olivia = User.find_or_create_by!(email: "olivia@example.com") do |user|
+  user.name = "Olivia"
+  user.password = "password"
+end
+
+james = User.find_or_create_by!(email: "james@example.com") do |user|
+  user.name = "James"
+  user.password = "password"
+end
+
+lucas = User.find_or_create_by!(email: "lucas@example.com") do |user|
+  user.name = "Lucas"
+  user.password = "password"
+end
+
+Post.find_or_create_by!(title: "やること") do |post|
+  post.user = olivia
+end
+
+Post.find_or_create_by!(title: "買い物リスト") do |post|
+  post.user = james
+end
+
+Post.find_or_create_by!(title: "勉強順") do |post|
+  post.user = lucas
+end
